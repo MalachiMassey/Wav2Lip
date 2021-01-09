7d8495e26b7e64a4455a853271715118e822484c
Malachi <57330523+MalachiMassey@users.noreply.github.com>
2021-01-09 14:31:45 -0600

    Update inference.py

--- inference.py
+++ inference.py
@@ -49,7 +49,7 @@ parser.add_argument('--rotate', default=False, action='store_true',
 
 parser.add_argument('--nosmooth', default=False, action='store_true',
 					help='Prevent smoothing face detections over a short temporal window')
-parser.addargument('--ignorefaceless', default=False, action='store_true', help='Ignore faceless frames.')
+parser.add_argument('--ignorefaceless', default=False, action='store_true', help='Ignore faceless frames.')
 
 
 args = parser.parse_args()
@@ -89,13 +89,14 @@ def face_detect(images):
 	results = []
 	pady1, pady2, padx1, padx2 = args.pads
 	for rect, image in zip(predictions, images):
-		if rect is None:
+		if rect is None && args.ignorefaceless:
 			y1 = max(0, 0)
 			y2 = min(0, 0)
 			x1 = max(0, 0)
 			x2 = min(0, 0)
-			#cv2.imwrite('temp/faulty_frame.jpg', image) # check this frame where the face was not detected.
-			#raise ValueError('Face not detected! Ensure the video contains a face in all the frames.')
+		elif rect is None:
+			cv2.imwrite('temp/faulty_frame.jpg', image) # check this frame where the face was not detected.
+			raise ValueError('Face not detected! Ensure the video contains a face in all the frames.')
 		else:			
 			y1 = max(0, rect[1] - pady1)
 			y2 = min(image.shape[0], rect[3] + pady2)
